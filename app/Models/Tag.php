143136e0d98b2aec922e01942744cfe2a97580d9
MisterT-Cell <tayyarcelik100@gmail.com>
2026-03-23 09:59:44 +0100

    Last changes

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Job;
 
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+
+        public function jobs()
+    {
+        return $this->belongsToMany(Job::class);
+    }
+
 }
